125e748745d520b802e13a242f1e871a6b49297a
TKachor <tkachor@gmail.com>
2021-06-07 22:27:06 +0300

    Add i projekt EarningModel

---
+++ FinanceWebApp/Components/EarningModel.cs
@@ -0,0 +1,19 @@
+﻿using FinanceWebApp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinanceWebApp.Components
+{
+    public class EarningModel
+    {
+        public DateTime Date { get; set; }
+
+        public string Subject { get; set; }
+
+        public EarningCategory Category { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
