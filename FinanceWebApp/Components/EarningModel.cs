5d7f637964ea08b647eb1bb74434a597bf907345
TKachor <tkachor@gmail.com>
2021-07-14 21:25:34 +0300

    Add validation to earninig form

--- FinanceWebApp/Components/EarningModel.cs
+++ FinanceWebApp/Components/EarningModel.cs
@@ -1,6 +1,7 @@
 ﻿using FinanceWebApp.Shared;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +9,14 @@ namespace FinanceWebApp.Components
 {
     public class EarningModel
     {
+        [Required]
         public DateTime Date { get; set; }
-
+        [Required]
+        [StringLength(50)]
         public string Subject { get; set; }
-
+        [Required]
         public EarningCategory Category { get; set; }
-
+        [Required]
         public decimal Amount { get; set; }
     }
 }
